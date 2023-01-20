e2a50265fce65ee3f2e28aba63eddce9e2c8def8
umasoya <umasoya.0331@gmail.com>
2023-01-20 15:26:05 +0900

    restructure

--- pokemonDictionaryGenerator.sh
+++ pokemonDictionaryGenerator.sh
@@ -8,51 +8,26 @@
 set -eu
 
 # load modules
-source __vars
-source __validate
-source __option
-source __print
-source __utils
-source __debug
+source modules/__vars
+source modules/__validate
+source modules/__options
+source modules/__print
+source modules/__utils
+source modules/__core
+source modules/__debug
 
 if "${DEBUG}"; then
   __debug::var_dump
 fi
 
+# validate option args
+__validate::integer ${FROM} f
+__validate::integer ${TO} t
 __validate::range ${FROM} ${TO} ${MAX_GEN}
 
-for GEN in $(seq ${FROM} ${TO}); do
-  if "${VERBOSE}" || "${DEBUG}"; then
-    CURL_OPTION=''
-  else
-    CURL_OPTION='-s'
-  fi
+URLS=()
+__core::fetch_urls ${FROM} ${TO}
 
-  __print::verbose "curl ${API}/generation/${GEN} | jq -r '.pokemon_species[] | .url' | sort -V"
-  URLS=$(curl ${CURL_OPTION} "${API}/generation/${GEN}/" | jq -r '.pokemon_species[] | .url' | sort -V)
-
-  if "${BUNDLE}"; then
-    FILENAME=${OUTPUT}
-  else
-    FILENAME="${OUTPUT}_gen${GEN}"
-  fi
-
-  i=0
-  for URL in ${URLS}; do
-    __utils::ifs_backup && __utils::ifs_newline
-    __print::verbose "curl ${CURL_OPTION} ${URL} | jq -r '.names[] | select(.language.name == \"ja\") | .name'"
-    __utils::ifs_reset
-
-    __utils::progress ${i} $(echo ${URLS} | tr " " "\n" | wc -l)
-
-    NAME=$(curl ${CURL_OPTION} "${URL}" | jq -r '.names[] | select(.language.name == "ja") | .name')
-    YOMIGANA=$(echo ${NAME} | uconv -x hiragana)
-    ROW="${YOMIGANA}\t${NAME}\t${CLASS}*"
-
-    __print::toFile ${ROW} ${FILENAME}
-    ((i++))
-  done
-  __utils::progress ${i} $(echo ${URLS} | tr " " "\n" | wc -l)
-done
+__core::fetch_pokemon
 
 exit 0
