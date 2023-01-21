e29c1bb72565f3865641cfb43ce17b850cbbef41
umasoya <umasoya.0331@gmail.com>
2023-01-22 02:37:29 +0900

    wip buffering

--- pokemonDictionaryGenerator.sh
+++ pokemonDictionaryGenerator.sh
@@ -10,17 +10,13 @@ set -eu
 #
 # load modules
 #
-
-# must be first
 source modules/__vars
-# no order
 source modules/__buffer
+source modules/__validator
+source modules/__options
 source modules/__debug
 source modules/__formatter
-source modules/__options
 source modules/__print
-source modules/__validator
-# must be last
 source modules/__utils
 source modules/__core
 
@@ -36,6 +32,15 @@ __validator::range ${FROM} ${TO} ${MAX_GEN}
 # stores value in ${URLS}
 __core::fetch_urls ${FROM} ${TO}
 
-__core::execute
+# store pokemons data in ${POKEMONS}
+__core::fetch_pokemons
+
+__core::generate_filenames
+
+__formatter::atok
+
+__core::write
+
+#__core::execute
 
 exit 0
