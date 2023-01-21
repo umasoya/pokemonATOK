e901b42fb0e3f71c2704b9e5a771eb090542b72d
umasoya <umasoya.0331@gmail.com>
2023-01-21 10:10:39 +0900

    rename validate

--- pokemonDictionaryGenerator.sh
+++ pokemonDictionaryGenerator.sh
@@ -7,25 +7,30 @@
 
 set -eu
 
+#
 # load modules
+#
+
+# no order
 source modules/__vars
-source modules/__validate
 source modules/__options
 source modules/__print
+source modules/__formatter
+source modules/__validator
+source modules/__debug
+# must be last
 source modules/__utils
 source modules/__core
-source modules/__debug
 
-if "${DEBUG}"; then
-  __debug::var_dump
-fi
+__debug::var_dump
 
+#
 # validate option args
-__validate::integer ${FROM} f
-__validate::integer ${TO} t
-__validate::range ${FROM} ${TO} ${MAX_GEN}
+#
+__validator::integer ${FROM} f
+__validator::integer ${TO} t
+__validator::range ${FROM} ${TO} ${MAX_GEN}
 
-URLS=()
 __core::fetch_urls ${FROM} ${TO}
 
 __core::fetch_pokemon
